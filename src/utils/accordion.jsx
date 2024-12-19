28c8eb6da47d9b06817c3f692b1b880f4b348705
neev sharma <neevsharma828@gmail.com>
2024-12-19 10:40:31 +0530

    first commit

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,24 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      
+      "Currently, the top savings account annual percentage yields (APY) range from 4.50 percent to 5.10 percent, with just a few companies still offering 5 percent or higher. .",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+    "Forecast For Fluctuations First and foremost, the business must forecast the financial implications of possible price fluctuations. ...",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Exercise in fleeing is to desire to it as in desire to blind as to blind the result is the least the least to be time labors result to be adipisicing eu reprehend for.",
+  },
+];
+export default data;
